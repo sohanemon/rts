8660b5cf681e1269ee6c60c7e0de5167ce4f5612
sohanemon <m.sohanemon@gmail.com>
2023-08-09 13:25:59 +0600

    feat: fonts

---
+++ src/app/layout.jsx
@@ -0,0 +1,32 @@
+import { Navbar } from '~/components/Navbar';
+import './globals.css';
+
+import localFont from 'next/font/local';
+
+const nova = localFont({
+  src: '../fonts/Regulator Nova.otf',
+  display: 'swap',
+  variable: '--nova',
+});
+const roc = localFont({
+  src: '../fonts/roc grotesk.ttf',
+  display: 'swap',
+  variable: '--roc',
+});
+
+export const metadata = {
+  title: 'Retain to Sustain',
+  description:
+    'A Comprehensive Closed-loop Approach to Sustainability & Circular Economy',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body className={`${nova.className} ${nova.variable} ${roc.variable}`}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
-  content: ['app/**/*.{ts,tsx}', 'components/**/*.{ts,tsx}'],
+  content: ['src/**/*.{js,jsx}'],
   theme: {
     container: {
       center: true,
@@ -12,8 +12,8 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        nunito: 'var(--nunito)',
-        canela: 'var(--canela)',
+        nova: 'var(--nova)',
+        roc: 'var(--roc)',
       },
       colors: {
         border: 'hsl(var(--border))',
