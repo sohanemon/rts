eb084aafc40ddbe1a369235ac55088d3c990f5b1
sohanemon <m.sohanemon@gmail.com>
2023-08-10 09:21:50 +0600

    added horizontal scrolling for latest article

--- src/app/(home)/LatestArticle.jsx
+++ src/app/(home)/LatestArticle.jsx
@@ -1,13 +1,19 @@
+'use client';
 import Article from '~/components/article';
+import { useHorizontalScroll } from '~/hook/scroll';
 
 export default function LatestArticle() {
+  const scrollRef = useHorizontalScroll();
   return (
     <section className='w-full bg-zinc-100 py-36 lg:px-32'>
       <div className='container'>
         <div className='w-[393px] mb-10 text-slate-900 text-[44px] font-bold leading-[56px]'>
           Latest articles
         </div>
-        <div className='flex gap-10 pb-8 overflow-x-auto cursor-grab active:cursor-grabbing flex-nowrap '>
+        <div
+          ref={scrollRef}
+          className='flex gap-10 pb-8 overflow-x-auto flex-nowrap '
+        >
           <Article />
           <Article />
           <Article />
