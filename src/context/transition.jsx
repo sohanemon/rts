6682a61d8b2a20b959abba83c3cecacffa9825bb
sohanemon <m.sohanemon@gmail.com>
2023-08-15 20:28:40 +0600

    feat: transition

--- src/context/transition.jsx
+++ src/context/transition.jsx
@@ -2,22 +2,26 @@
 
 import { AnimatePresence } from 'framer-motion';
 import { usePathname } from 'next/navigation';
-
-import Motion from '../components/motion';
+import { motion } from 'framer-motion';
+import { useRef } from 'react';
+import Motion from '~/components/motion';
 
 export default function TransitionProvider({ children }) {
   const path = usePathname();
+  const containerRef = useRef(null);
   return (
-    <AnimatePresence initial={false} mode='wait'>
-      <Motion
+    <AnimatePresence initial={false}>
+      <motion.div
+        ref={containerRef}
         initial={{ x: -1000, opacity: 0 }}
-        animate='visible'
+        animate={{ x: 0, opacity: 1 }}
         transition={{ delay: 0, duration: 0.4 }}
-        exit={{ x: 1000, overflow: 'hidden', opacity: 0 }}
+        exit={{ x: 1000, opacity: 0 }}
         key={path}
       >
         {children}
-      </Motion>
+        <Motion />
+      </motion.div>
     </AnimatePresence>
   );
 }
